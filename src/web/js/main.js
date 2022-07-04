7b25ecf674a1776650830525e70ac3db49291930
Ryan <ryan.lo@polymtl.ca>
2022-07-03 20:03:06 -0400

    grid dragsort done

--- src/web/js/main.js
+++ src/web/js/main.js
@@ -1,32 +1,23 @@
-import { addPageToGallery } from "./modules/gallery.js"; 
-const url = '../../1A_structure.pdf';
+import { convertDataToTiles } from "./modules/gallery.js";
+const url = "../../1A_structure.pdf";
 
 let pdfDoc = null,
-nPages = 0,
-pdfIsRendering = false;
+  nPages = 0,
+  pdfIsRendering = false;
 
-pdfjsLib.getDocument(url).promise.then(pdfDoc_ =>{
-	pdfDoc = pdfDoc_;
-	nPages = pdfDoc.numPages;
-	loadPdf();
-})
+pdfjsLib.getDocument(url).promise.then((pdfDoc_) => {
+  pdfDoc = pdfDoc_;
+  nPages = pdfDoc.numPages;
+  loadPdf();
+});
 
 //render the PDF
-const loadPdf = async () => {
-	let promises = []; 
-	for (let i = 0; i < nPages; i ++){
-		promises.push(pdfDoc.getPage(i+1))
-		console.log(pdfDoc.getPage(1));
-	}
-	const data = await Promise.all(promises);
-	let i = 0;
-	data.forEach((page) =>{
-		addPageToGallery(page);
-	})
-	 
-};
-
-
-
-
-
+async function loadPdf() {
+  let promises = [];
+  for (let i = 0; i < nPages; i++) {
+    promises.push(pdfDoc.getPage(i + 1));
+    console.log(pdfDoc.getPage(1));
+  }
+  const data = await Promise.all(promises);
+  convertDataToTiles(data);
+}
--- src/web/js/modules/gallery.js
+++ src/web/js/modules/gallery.js
@@ -1,92 +1,147 @@
-var tiles = [];
-const addPageToGallery = (page) => {
+let tiles = [];
+let dragStartIndex;
+let dragEndIndex;
+let source;
 
-	//create canvas
-	let canvas = document.createElement('canvas');
-	let div = document.createElement('div');
-	div.className = "canvas-container";
-	div.appendChild(canvas);
-	div.setAttribute('draggable', true);
-	let gallery = document.getElementById("gallery");
-	gallery.appendChild(div);
-	
-	//tracking
-	// div.index = index;
-	// tiles.push(div);
+function createTile(page) {
+  //create canvas
+  let canvas = document.createElement("canvas");
+  let canvasContainer = document.createElement("div");
+  canvasContainer.className = "canvas-container";
+  canvasContainer.appendChild(canvas);
+  canvasContainer.setAttribute("draggable", true);
+  applyDragBehaviorToElement(canvasContainer);
 
-	//apply drag behavior
-	div.onmousedown = (e) => onMouseDown(e);
-	div.ondragenter = (e) => onDragEnter(e);
-	div.ondragstart = (e) => onDragStart(e);
-	div.ondragover = (e) =>{
-		e.preventDefault();
-		e.dataTransfer.dropEffect = "move";
-	}
-	//div.onmouseup = (e) => onMouseUp(e);
-	
-	//adjust scaling
-	var desiredWidth = 120;
-	var viewport = page.getViewport({ scale: 1, });
-	var scale = desiredWidth / viewport.width;
-	var scaledViewport = page.getViewport({ scale: scale, });
+  //adjust scaling
+  var desiredWidth = 120;
+  var viewport = page.getViewport({ scale: 1 });
+  var scale = desiredWidth / viewport.width;
+  var scaledViewport = page.getViewport({ scale: scale });
 
-	canvas.height = scaledViewport.height;
-	canvas.width = scaledViewport.width;
-	let ctx = canvas.getContext('2d')
-	
-	
-	const renderCtx = {
-		canvasContext: ctx,
-		viewport : scaledViewport,
-	}
-	page.render(renderCtx);
+  canvas.height = scaledViewport.height;
+  canvas.width = scaledViewport.width;
+  let ctx = canvas.getContext("2d");
 
-}
-var source;
+  const renderCtx = {
+    canvasContext: ctx,
+    viewport: scaledViewport,
+  };
+  page.render(renderCtx);
 
-const onMouseDown = (e) =>{
-	source = e.currentTarget;
-	e.style.cursor = 'move';
-	e.preventDefault();
+  let tile = document.createElement("div");
+  tile.appendChild(canvasContainer);
+  return tile;
 }
 
-const onDragStart = (e) =>{
-	e.dataTransfer = "move";
-	//e.preventDefault()
-	
+function convertDataToTiles(data) {
+  let fragment = document.createDocumentFragment();
+  data.forEach((page) => {
+    let tile = createTile(page);
+    tiles.push(tile);
+    //indexing
+    tile.setAttribute("data-index", tiles.length - 1);
+    tile.addEventListener("drop", onDragDrop);
+    tile.addEventListener("dragover", onDragOver);
+    fragment.appendChild(tile);
+  });
+  let gallery = document.getElementById("gallery");
+  gallery.appendChild(fragment);
 }
 
-
-const onDragEnter = (e) =>{
-	if(source != e.currentTarget){
-		let target = e.currentTarget;
-		target = target !== source.nextSibling? 
-		target :
-		target.nextSibling;
-		gallery.insertBefore(source, target	);
-	}
-	// console.log("enter");
-	// e.preventDefault();
-	// if(source !== e.currentTarget){
-	//  	changePositions(source, e.currentTarget);
-	// }
-	//e.target.parentNode.insertBefore(source, e.target)	
+function applyDragBehaviorToElement(elem) {
+  //apply drag behavior
+  elem.ondragstart = (e) => onDragStart(e);
+  elem.ondragenter = (e) => onDragEnter(e);
+  elem.ondragover = (e) => onDragOver(e);
+  elem.ondragend = (e) => changePositions(dragStartIndex, dragEndIndex);
 }
 
-
-
-const changePositions = (source, target)=> {
-	// if(source.index > target.index){
-	// 	gallery.insertBefore(source, target)
-	// 	let temp = tiles.splice(source, 1);
-	// 	tiles.splice(target, 0, temp[0]);
-	// 	for(let i = target.index; i < source.index; i++){
-	// 		tiles[i].index++;
-	// 	}
-	// 	source.index = target.index-1;
-		
-	// }
-	
+function onDragStart(e) {
+  dragStartIndex = e.target.parentElement.getAttribute("data-index");
+  source = e.target;
+  source.className = "selected canvas-container"
 }
 
-export{ addPageToGallery }
+function onDragEnter(e) {
+  e.preventDefault();
+  dragEndIndex = parseInt(e.currentTarget.parentElement.getAttribute("data-index"));
+  shiftPositions(dragStartIndex, dragEndIndex);
+  //e.target.parentNode.insertBefore(source, e.target)
+}
+
+function onDragOver(e) {
+  e.preventDefault();
+  e.dataTransfer.effectAllowed = "move";
+  e.dataTransfer.dropEffect = "copy";
+}
+
+function onDragDrop(e) {
+  //e.preventDefault();
+  dragEndIndex = parseInt(e.currentTarget.getAttribute("data-index"));
+}
+
+function changePositions(fromIndex, toIndex) {
+  source.className = "canvas-container"
+  let temp = tiles.splice(fromIndex, 1);
+  tiles.splice(toIndex, 0, temp[0]);
+  let fragment = document.createDocumentFragment();
+  let i = 0;
+  tiles.forEach((tile) => {
+    tile.setAttribute("data-index", i++);
+    tile.firstChild.style.transform = "";
+    fragment.appendChild(tile);
+  });
+  let gallery = document.getElementById("gallery");
+  gallery.replaceChildren(fragment);
+}
+
+function shiftPositions(fromIndex, toIndex) {
+  if (fromIndex < toIndex) {
+    shiftLeft(fromIndex, toIndex);
+  } else {
+    document.querySelectorAll(".canvas-container").forEach((tile) => {
+      var tileIndex = parseInt(tile.parentElement.getAttribute("data-index"));
+      let width = document
+        .getElementById("gallery")
+        .getBoundingClientRect().width;
+      let x = 0;
+      let y = 0;
+      if (fromIndex == toIndex) {
+        x = 0;
+        y = 0;
+      } else if (tileIndex < fromIndex && tileIndex >= toIndex) {
+        x = 130;
+        let boundingRect = tile.getBoundingClientRect();
+        let nColumns = Math.trunc(width / 130);
+        if ((tileIndex + 1) % nColumns == 0) {
+          x = -(nColumns - 1) * 130;
+          y = boundingRect.height;
+        }
+      }
+      tile.style.transform = `translate(${x}px, ${y}px)`;
+    });
+  }
+}
+
+function shiftLeft(fromIndex, toIndex) {
+  document.querySelectorAll(".canvas-container").forEach((tile) => {
+    var tileIndex = parseInt(tile.parentElement.getAttribute("data-index"));
+    let width = document
+      .getElementById("gallery")
+      .getBoundingClientRect().width;
+    let x = 0;
+    let y = 0;
+    if (tileIndex > fromIndex && tileIndex <= toIndex) {
+      x = -130;
+      let boundingRect = tile.getBoundingClientRect();
+      let nColumns = Math.trunc(width / 130);
+      if (tileIndex % nColumns == 0) {
+        x = (nColumns - 1) * 130;
+        y = -boundingRect.height;
+      }
+    }
+    tile.style.transform = `translate(${x}px, ${y}px)`;
+  });
+}
+
+export { createTile, convertDataToTiles };
