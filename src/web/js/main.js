e24b5e0b777b57fa71c7609e6c7c5beac9a6e7a7
Ryan <ryan.lo@polymtl.ca>
2022-11-16 21:33:31 -0500

    change input ui

--- src/web/js/main.js
+++ src/web/js/main.js
@@ -1,70 +1,42 @@
+import PdfReader from "./fileLoader/fileLoader.js";
 import Gallery from "./gallery/gallery.js";
 
 let fileInput = document.getElementById("file-input");
+let fileUpload = document.getElementById("file-upload")
+let saveButton = document.getElementById("save-button")
 
+document.getElementById("browse").addEventListener("click", browseFiles);
 document.getElementById("save-button").addEventListener("click", savePdf);
+
 fileInput.onchange = (event) => {
+  if(fileUpload.style.display != "flex"){
+    fileUpload.style.display = "none"
+  }
   loadFiles(event.target.files);
 };
 
 let gallery = new Gallery("gallery");
+let pdfReader = new PdfReader();
 
-function loadFiles(files) {
-  let readers = [];
-  //Read all inputs
-  Array.from(files).forEach((file) => {
-    readers.push(readFileAsArrayBuffer(file));
-  });
-  //Await all results and load each document
-  Promise.all(readers).then((readResults) =>{
-    readResults.forEach(result => loadDocument(new Uint8Array(result)));
+
+function browseFiles(){
+  fileInput.click();
+}
+
+async function loadFiles(files) {
+  const readersResults = await pdfReader.readFilesAsArrayBuffer(files);
+  readersResults.forEach(async arrayBuffer => {
+    let id = await eel.saveArrayBuffer(Array.from(arrayBuffer))();
+    const pages = await pdfReader.loadDocumentPages(arrayBuffer, id);
+    const pdfInfo = {
+      pdfId: id,
+      pages: pages,
+    };
+    gallery.addPdf(pdfInfo);
   });
 }
 
 function savePdf() {
   let payload = gallery.buildSavePayload();
-  console.log(payload);
   eel.savePdf(payload);
-}
-
-
-async function loadDocument(arrayBuffer) {
-  return new Promise(async (resolve, reject) => {
-    try {
-      let id = await eel.saveArrayBuffer(Array.from(arrayBuffer))();
-      console.log(id);
-      const loadingTask = pdfjsLib.getDocument(arrayBuffer);
-      const pdfDoc = await loadingTask.promise;
-      const pdf = {
-        pdfId : id,
-        pages: await loadPages(pdfDoc),
-      }
-      gallery.addPdf(pdf);
-      resolve();
-    } catch (e) {
-      reject(e);
-    }
-  });
-}
-
-async function loadPages(pdf) {
-  let promises = [];
-  for (let i = 0; i < pdf.numPages; i++) {
-    promises.push(pdf.getPage(i + 1));
-  }
-  return Promise.all(promises);
-}
-
-function readFileAsArrayBuffer(file){
-  return new Promise((resolve, reject)=>{
-    let filereader = new FileReader();
-    
-    filereader.onload = () => {
-      resolve(filereader.result);
-    };
-
-    filereader.onerror = () => reject(filereader);
-
-    filereader.readAsArrayBuffer(file);
-  })
 }
